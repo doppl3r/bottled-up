6517cae3255334928f16f4f989149015001afe5d
Jacob DeBenedetto <deben3@gmail.com>
2026-08-01 10:02:49 -0700

    Refactor EntityCube

--- src/js/core/EntityCube.js
+++ src/js/core/EntityCube.js
@@ -1,4 +1,14 @@
+import { Box3, Vector3 } from 'three';
 import { Entity } from './Entity.js';
+
+/*
+  EntityCube is a specialized entity that represents a cube-shaped object
+  with a corresponding physics collider. It automatically adjusts its scale
+  based on the size of the associated model.
+*/
+
+// Initialize module-scoped variables
+const _v = new Vector3();
 
 class EntityCube extends Entity {
   constructor(options) {
@@ -12,13 +22,49 @@ class EntityCube extends Entity {
   }
 
   init(options, core) {
-    // Update cube options
-    options.children.forEach(child => {
-      if (child.class === 'EntityPhysics') {
+    // Update template for child entities
+    for (let i = 0; i < options.children.length; i++) {
+      // Update child entity options
+      let child = options.children[i];
+
+      // Update model properties
+      if (child.class === 'EntityModel') {
+        const model = core.assets.get(options.url);
+
+        // Check if model exists
+        if (model) {
+          child.url = options.url;
+        }
+        else {
+          // Fallback: Replace EntityModel with EntityMesh
+          options.children[i] = {
+            class: 'EntityMesh',
+            geometry: {
+              type: 'BoxGeometry',
+              arguments: [1, 1, 1]
+            },
+            material: {
+              type: 'MeshStandardMaterial',
+              arguments: [{ color: options.color }],
+            }
+          };
+        }
+      }
+      else if (child.class === 'EntityPhysics') {
         // Update transformations
         child.rigidBody.position = options.position;
         child.rigidBody.rotation = options.rotation;
 
+        // Update scale based on model size
+        const model = core.assets.get(options.url);
+        if (model) {
+          model.rotation.set(0, 0, 0);
+          const size = new Box3().setFromObject(model).getSize(_v);
+          options.scale.x = size.x;
+          options.scale.y = size.y;
+          options.scale.z = size.z;
+        }
+        
         // Update shape scale
         const colliders = child.rigidBody.colliders;
         colliders.forEach(colliderOptions => {
@@ -26,17 +72,18 @@ class EntityCube extends Entity {
           colliderOptions.shapeDesc.arguments = args;
         });
       }
-      else if (child.class === 'EntityMesh') {
-        // Update material color
-        child.material.arguments[0].color = options.color;
-      }
-    });
+    }
 
+    // Resume base entity initialization
     super.init(options, core);
   }
 
   static template = {
-    color: '#ffffff',
+    color: '#ff00ff',
+    url: '',
+    position: { x: 0, y: 0, z: 0 },
+    rotation: { x: 0, y: 0, z: 0 },
+    scale: { x: 1, y: 1, z: 1 },
     children: [
       {
         class: 'EntityPhysics',
@@ -53,15 +100,7 @@ class EntityCube extends Entity {
         }
       },
       {
-        class: 'EntityMesh',
-        geometry: {
-          type: 'BoxGeometry',
-          arguments: [1, 1, 1]
-        },
-        material: {
-          type: 'MeshStandardMaterial',
-          arguments: [{ color: '#ffffff' }],
-        }
+        class: 'EntityModel'
       }
     ]
   }
