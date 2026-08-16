cb5aefe098cdd523d403d7ebe33ff87799c0d441
Jacob DeBenedetto <deben3@gmail.com>
2026-08-16 15:05:02 -0700

    Merge all npcs

--- src/js/EntityNPC.js
+++ src/js/EntityNPC.js
@@ -15,7 +15,7 @@ class EntityNPC extends Entity {
     children: [
       {
         class: 'EntityModel',
-        url: 'glb/npc-warrior.glb',
+        url: 'glb/npc-rogue.glb',
         position: { x: 0, y: -0.25, z: 0 },
         children: [
           {
