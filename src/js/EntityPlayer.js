75c400f404bc74b133db3bef62c24fd1517cd891
Jacob DeBenedetto <deben3@gmail.com>
2026-08-22 10:02:48 -0700

    Add more potion templates and models

--- src/js/EntityPlayer.js
+++ src/js/EntityPlayer.js
@@ -25,7 +25,75 @@ class EntityPlayer extends Entity {
     super.render(loop);
   }
 
-  static template = {
+  static templateCube = {
+    children: [
+      {
+        class: 'EntityBallController'
+      },
+      {
+        class: 'EntityModel',
+        url: 'glb/potion-cube.glb'
+      },
+      {
+        class: 'EntityMesh',
+        scale: { x: 0.4, y: 0.4, z: 0.4 },
+        position: { x: 0, y: 0, z: 0.25 },
+        castShadow: false,
+        receiveShadow: true,
+        renderOrder: 1,
+        geometry: {
+          type: 'PlaneGeometry',
+          arguments: [1, 1]
+        },
+        material: {
+          type: 'MeshStandardMaterial',
+          arguments: [{ color: '#ffffff', transparent: true }],
+        },
+        children: [
+          {
+            class: 'EntityTexture',
+            url: 'png/smile.png',
+            minFilter: 1003,
+            magFilter: 1003,
+            repeat: { x: 1, y: 1 },
+          }
+        ]
+      },
+      {
+        class: 'EntityPhysics',
+        rigidBody: {
+          status: 0,
+          softCcdPrediction: 1.0,
+          sleeping: true,
+          colliders: [
+            {
+              friction: 0,
+              shapeDesc: {
+                shapes: [
+                  {
+                    type: 'roundCuboid',
+                    arguments: [(1 / 4) - (1 / 8), (1 / 4) - (1 / 8), (1 / 4) - (1 / 8), (1 / 8)],
+                  },
+                  {
+                    type: 'ball',
+                    arguments: [0.125],
+                    translation: { x: 0, y: 0.25, z: 0 }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      {
+        class: 'EntityShadow',
+        distance: 64,
+        scale: { x: 0.5, y: 0.5, z: 0.5 }
+      }
+    ]
+  }
+
+  static templateBall = {
     children: [
       {
         class: 'EntityBallController'
@@ -91,6 +159,70 @@ class EntityPlayer extends Entity {
       }
     ]
   }
+
+  static templateCylinder = {
+    children: [
+      {
+        class: 'EntityBallController'
+      },
+      {
+        class: 'EntityModel',
+        url: 'glb/potion-cylinder.glb'
+      },
+      {
+        class: 'EntityMesh',
+        scale: { x: 1, y: 1, z: 1 },
+        castShadow: false,
+        receiveShadow: true,
+        renderOrder: 1,
+        geometry: {
+          type: 'CylinderGeometry',
+          arguments: [0.125, 0.125, 0.25, 12, 1, true, -Math.PI / 2, Math.PI]
+        },
+        material: {
+          type: 'MeshStandardMaterial',
+          arguments: [{ color: '#ffffff', transparent: true }],
+        },
+        children: [
+          {
+            class: 'EntityTexture',
+            url: 'png/smile.png',
+            minFilter: 1003,
+            magFilter: 1003,
+            repeat: { x: 1, y: 1 },
+            offset: { x: 0, y: 0 }
+          }
+        ]
+      },
+      {
+        class: 'EntityPhysics',
+        rigidBody: {
+          status: 0,
+          softCcdPrediction: 1.0,
+          sleeping: true,
+          colliders: [
+            {
+              shapeDesc: {
+                shapes: [
+                  {
+                    type: 'roundCylinder',
+                    arguments: [(1 / 4) - (1 / 16), (1 / 16), (1 / 16)],
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      {
+        class: 'EntityShadow',
+        distance: 64,
+        scale: { x: 0.5, y: 0.5, z: 0.5 }
+      }
+    ]
+  }
+
+  static template = this.templateBall;
 }
 
 export { EntityPlayer }
