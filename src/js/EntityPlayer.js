4e0693c35f4ba208d01a1c4fd72c179da359cdb5
Jacob DeBenedetto <deben3@gmail.com>
2026-08-22 13:04:11 -0700

    Add cone shape

--- src/js/EntityPlayer.js
+++ src/js/EntityPlayer.js
@@ -41,7 +41,7 @@ class EntityPlayer extends Entity {
 
   static applyTemplate(options) {
     // TODO: Remove shape override
-    options.shape = 'ball';
+    options.shape = 'capsule';
 
     // Update template based on shape
     this.template = this.templates[options.shape];
@@ -184,14 +184,14 @@ class EntityPlayer extends Entity {
         }
       ]
     },
-    cylinder: {
+    capsule: {
       children: [
         {
           class: 'EntityBallController'
         },
         {
           class: 'EntityModel',
-          url: 'glb/potion-cylinder.glb'
+          url: 'glb/potion-capsule.glb'
         },
         {
           class: 'EntityMesh',
@@ -213,8 +213,8 @@ class EntityPlayer extends Entity {
               url: 'png/smile.png',
               minFilter: 1003,
               magFilter: 1003,
-              repeat: { x: 1, y: 1 },
-              offset: { x: 0, y: 0 }
+              repeat: { x: 1.5, y: 1 },
+              offset: { x: -0.25, y: 0 }
             }
           ]
         },
@@ -229,8 +229,79 @@ class EntityPlayer extends Entity {
                 shapeDesc: {
                   shapes: [
                     {
-                      type: 'roundCylinder',
-                      arguments: [(1 / 4) - (1 / 16), (1 / 16), (1 / 16)],
+                      type: 'capsule',
+                      arguments: [(1 / 4) - (1 / 8), (1 / 8)],
+                    },
+                    {
+                      type: 'ball',
+                      arguments: [0.125],
+                      translation: { x: 0, y: 0.25, z: 0 }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        },
+        {
+          class: 'EntityShadow',
+          distance: 64,
+          scale: { x: 0.5, y: 0.5, z: 0.5 }
+        }
+      ]
+    },
+    cone: {
+      children: [
+        {
+          class: 'EntityBallController'
+        },
+        {
+          class: 'EntityModel',
+          url: 'glb/potion-cone.glb'
+        },
+        {
+          class: 'EntityMesh',
+          scale: { x: 1, y: 1, z: 1 },
+          castShadow: false,
+          receiveShadow: true,
+          renderOrder: 1,
+          geometry: {
+            type: 'CylinderGeometry',
+            arguments: [(1 / 16) + (1 / 16), (1 / 8) + (1 / 16), 0.25, 12, 1, true, -Math.PI / 2, Math.PI]
+          },
+          material: {
+            type: 'MeshStandardMaterial',
+            arguments: [{ color: '#ffffff', transparent: true }],
+          },
+          children: [
+            {
+              class: 'EntityTexture',
+              url: 'png/smile.png',
+              minFilter: 1003,
+              magFilter: 1003,
+              repeat: { x: 1.5, y: 1 },
+              offset: { x: -0.25, y: 0 }
+            }
+          ]
+        },
+        {
+          class: 'EntityPhysics',
+          rigidBody: {
+            status: 0,
+            softCcdPrediction: 1.0,
+            sleeping: true,
+            colliders: [
+              {
+                shapeDesc: {
+                  shapes: [
+                    {
+                      type: 'roundCone',
+                      arguments: [(1 / 4) - (1 / 16), (1 / 8), (1 / 16)],
+                    },
+                    {
+                      type: 'ball',
+                      arguments: [0.125],
+                      translation: { x: 0, y: 0.25, z: 0 }
                     }
                   ]
                 }
